88540085d930f84ce7e72f6674d468e0de0ad6c4
Estefanía Saldarriaga Muñoz <128753905+EstefaSM@users.noreply.github.com>
2024-05-07 02:42:22 -0500

    Se agregan estilos en los componentes Main, Services, Asideright

--- src/components/AsideRight.js
+++ src/components/AsideRight.js
@@ -1,105 +1,49 @@
 import React from 'react';
-import avatarOne from '../img/avatar1.webp';
-// import './styles.css';
-
+import avatarOne from '../img/avatarOne.jpg';
+import avatarTwo from '../img/avatarTwo.jpg';
+import avatarThree from '../img/avatarThree.jpg'
 
 const AsideRight = () => {
     return (
-      <div className="d-flex justify-content-end">
-        <div className="card-container">
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
+      <div className="testimonials">
+          <div className="container-fluid">
+              <div className="row">
+                <h4>Opiniones</h4>
+                <hr/>
+                  <div className="col-md-3 mt-1">
+                      <img src={avatarOne} className="card-img-top"  alt="Foto del testimonio" />
+                    </div>
+                    <div className="col-md-8 mt-1">
+                        <div className="card-body">
+                          <h5 className="card-title">María</h5>
+                          <p>Residente en Medellín</p>
+                          <p className="card-text" > <small>"¡Qué gran herramienta! Gracias a este sitio web, puedo ver los niveles de contaminación en mi área y tomar medidas para proteger la salud de mi familia. La visualización de datos es clara y fácil de entender."</small></p>
+                        </div>
+                    </div>  
+                    <hr/>
+                  <div className="col-md-3 mt-1">
+                      <img src={avatarTwo} className="card-img-top" alt="Foto del testimonio" />
+                    </div>
+                    <div className="col-md-8 mt-1">
+                        <div className="card-body">
+                          <h5 className="card-title">Carlos</h5>
+                          <p>Urbanista</p>
+                          <p className="card-text"><small>"Como urbanista, necesito acceso a datos precisos sobre el uso del suelo y la deforestación para diseñar proyectos sostenibles en el Valle de Aburrá. Este sitio web me proporciona la información que necesito de manera rápida y eficiente."</small></p>
+                        </div>
+                    </div>  
+                    <hr/>
+                  <div className="col-md-3 mt-1">
+                      <img src={avatarThree} className="card-img-top" alt="Foto del testimonio" />
+                    </div>
+                    <div className="col-md-8 mt-1">
+                        <div className="card-body">
+                          <h5 className="card-title">Luisa</h5>
+                          <p>Estudiante de Biología</p>
+                          <p className="card-text"><small>"Estoy estudiando biología y me interesa mucho la conservación de la biodiversidad en nuestra región. Puedo acceder a datos actualizados sobre la distribución de especies y los cambios en los ecosistemas, lo que me permite realizar análisis detallados y contribuir al conocimiento científico sobre nuestro entorno."</small></p>
+                        </div>
+                    </div>  
               </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
           </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img src={avatarOne} className="card-img-top" alt="Foto del testimonio" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">Nombre del Testimonio</h5>
-                  <p className="card-text">Texto del testimonio</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Repite las tarjetas adicionales según sea necesario */}
-        </div>
       </div>
     );
   };
